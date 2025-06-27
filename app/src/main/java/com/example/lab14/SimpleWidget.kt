26557c27e2bc4d5bc99effe60f11f53e68e68c58
MarrVL <mariel.valdez@tecsup.edu.pe>
2025-06-26 21:00:21 -0500

    Prueba de Widget

--- app/src/main/java/com/example/lab14/SimpleWidget.kt
+++ app/src/main/java/com/example/lab14/SimpleWidget.kt
@@ -1,4 +1,44 @@
 package com.example.lab14
 
-class SimpleWidget {
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import androidx.glance.*
+import androidx.glance.action.actionStartActivity
+import androidx.glance.appwidget.provideContent
+import androidx.glance.layout.*
+import androidx.glance.text.Text
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
+class SimpleWidgetContent : GlanceAppWidget() {
+    override suspend fun provideGlance(context: Context, id: GlanceId) {
+        provideContent {
+            GlanceTheme {
+                MyContent()
+            }
+        }
+    }
+
+    @Composable
+    private fun MyContent() {
+        Column(
+            modifier = GlanceModifier.fillMaxSize()
+                .background(GlanceTheme.colors.background),
+            verticalAlignment = Alignment.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "¿A dónde quieres dirigirte?", modifier = GlanceModifier.padding(12.dp))
+            Row(horizontalAlignment = Alignment.CenterHorizontally) {
+                Button(
+                    text = "Página Principal",
+                    onClick = actionStartActivity<MainActivity>()
+                )
+            }
+        }
+    }
+}
+
+class SimpleWidget : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget = SimpleWidgetContent()
 }
